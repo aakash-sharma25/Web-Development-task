528147e8f2b9267973f7a3f54295a824d40da049
aakash-sharma25 <as024harma@gmail.com>
2024-02-23 16:19:47 +0530

    Added the data.js file

---
+++ data.js
@@ -0,0 +1,127 @@
+const imageData = [
+    {
+      image: "./assets/doctor_1.png",
+      name: "Dr. Emily Rodriguez",
+      specialization: "Dentist",
+    },
+    {
+      image: "./assets/doctor_2.png",
+      name: "Dr. Jonathan Miller",
+      specialization: "Cardiologist",
+    },
+    {
+      image: "./assets/doctor_3.png",
+      name: "Dr. Sophia Chang",
+      specialization: "Dermatologist",
+    },
+    {
+      image: "./assets/doctor_4.png",
+      name: "Dr. Nawaz Joharji",
+      specialization: "Pediatrician",
+    },
+    {
+      image: "./assets/doctor_1.png",
+      name: "Dr. Emily Rodriguez",
+      specialization: "Dentist",
+    },
+    {
+      image: "./assets/doctor_2.png",
+      name: "Dr. Jonathan Miller",
+      specialization: "Cardiologist",
+    },
+    {
+      image: "./assets/doctor_3.png",
+      name: "Dr. Sophia Chang",
+      specialization: "Dermatologist",
+    },
+    {
+      image: "./assets/doctor_4.png",
+      name: "Dr. Nawaz Joharji",
+      specialization: "Pediatrician",
+    },
+    {
+      image: "./assets/doctor_1.png",
+      name: "Dr. Emily Rodriguez",
+      specialization: "Dentist",
+    },
+    {
+      image: "./assets/doctor_2.png",
+      name: "Dr. Jonathan Miller",
+      specialization: "Cardiologist",
+    },
+    {
+      image: "./assets/doctor_3.png",
+      name: "Dr. Sophia Chang",
+      specialization: "Dermatologist",
+    },
+    {
+      image: "./assets/doctor_4.png",
+      name: "Dr. Nawaz Joharji",
+      specialization: "Pediatrician",
+    },
+    {
+      image: "./assets/doctor_1.png",
+      name: "Dr. Emily Rodriguez",
+      specialization: "Dentist",
+    },
+    {
+      image: "./assets/doctor_2.png",
+      name: "Dr. Jonathan Miller",
+      specialization: "Cardiologist",
+    },
+    {
+      image: "./assets/doctor_3.png",
+      name: "Dr. Sophia Chang",
+      specialization: "Dermatologist",
+    },
+    {
+      image: "./assets/doctor_4.png",
+      name: "Dr. Nawaz Joharji",
+      specialization: "Pediatrician",
+    },
+    {
+      image: "./assets/doctor_1.png",
+      name: "Dr. Emily Rodriguez",
+      specialization: "Dentist",
+    },
+    {
+      image: "./assets/doctor_2.png",
+      name: "Dr. Jonathan Miller",
+      specialization: "Cardiologist",
+    },
+    {
+      image: "./assets/doctor_3.png",
+      name: "Dr. Sophia Chang",
+      specialization: "Dermatologist",
+    },
+    {
+      image: "./assets/doctor_4.png",
+      name: "Dr. Nawaz Joharji",
+      specialization: "Pediatrician",
+    },
+    {
+      image: "./assets/doctor_1.png",
+      name: "Dr. Emily Rodriguez",
+      specialization: "Dentist",
+    },
+    {
+      image: "./assets/doctor_2.png",
+      name: "Dr. Jonathan Miller",
+      specialization: "Cardiologist",
+    },
+    {
+      image: "./assets/doctor_3.png",
+      name: "Dr. Sophia Chang",
+      specialization: "Dermatologist",
+    },
+    {
+      image: "./assets/doctor_4.png",
+      name: "Dr. Nawaz Joharji",
+      specialization: "Pediatrician",
+    },
+    {
+      image: "./assets/doctor_1.png",
+      name: "Dr. Emily Rodriguez",
+      specialization: "Dentist",
+    },
+  ];
